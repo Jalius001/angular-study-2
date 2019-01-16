e7fbd6f46edd00b98ebcad993e29dfdb428fd26d
Sergiy Rylskii <srylskii@luxoft.com>
2019-01-16 12:11:52 +0200

    Example 1 done (include starred task)

--- src/app/joke/joke.component.ts
+++ src/app/joke/joke.component.ts
@@ -1,8 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import {Component, Input, Output, OnInit, EventEmitter} from '@angular/core';
+import {Component, Input, Output, OnInit, EventEmitter, OnDestroy} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {Joke} from '../beans/Joke';
-import {JOKES} from '../mocks/MockJokes';
+import { Joke } from '../beans/Joke';
+import { JOKES } from '../mocks/MockJokes';
 import set = Reflect.set;
 
 @Component({
@@ -42,21 +42,21 @@ export class JokeFormComponent {
   selector: 'app-joke-item',
   template: `
     <div class="card card-block">
-      <h4 class="card-title">{{data.setup}}</h4>
+      <h4 class="card-title">{{joke.setup}}</h4>
       <p class="card-text"
-         [hidden]="data.hide">{{data.punchLine}}</p>
-      <a (click)="data.toggle()"
+         [hidden]="joke.hide">{{joke.punchLine}}</p>
+      <a (click)="joke.toggle()"
          class="btn btn-warning">Tell Me
       </a>
-      <button type="button" class="btn btn-secondary" (click)="starJoke(data)">Star</button>
-      <button type="button" class="btn btn-secondary" (click)="deleteJoke(data)">Delete</button>
+      <button type="button" class="btn btn-secondary" (click)="starJoke(joke)">Star</button>
+      <button type="button" class="btn btn-secondary" (click)="deleteJoke(joke)">Delete</button>
     </div>`,
   styleUrls: ['joke.component.styl']
 })
 export class JokeItemComponent {
   @Output() jokeDeleteEvent = new EventEmitter<Joke>();
   @Output() jokeStarEvent = new EventEmitter<Joke>();
-  @Input('joke') data: Joke;
+  @Input() joke: Joke;
 
   deleteJoke(joke: Joke) {
     this.jokeDeleteEvent.emit(joke);
@@ -71,14 +71,14 @@ export class JokeItemComponent {
   selector: 'app-joke-starred-item',
   template: `
     <div class="card card-block">
-      <h2 class="card-title">{{data.setup}}</h2>
-      <button type="button" class="btn btn-secondary" (click)="unstarJoke(data)">Unstar</button>
+      <h4 class="card-title">{{joke.setup}}</h4>
+      <button type="button" class="btn btn-secondary" (click)="unstarJoke(joke)">Unstar</button>
     </div>`,
   styleUrls: ['joke.component.styl']
 })
 export class JokeStarredItemComponent {
   @Output() jokeUnstarEvent = new EventEmitter<Joke>();
-  @Input('starredJoke') data: Joke;
+  @Input() joke: Joke;
 
   unstarJoke(joke: Joke) {
     this.jokeUnstarEvent.emit(joke);
@@ -88,55 +88,49 @@ export class JokeStarredItemComponent {
 @Component({
   selector: 'app-joke-starred-list',
   template: `
-    <div >
+    <div>
       <p>Starred Jokes:</p>
-      <app-joke-starred-item *ngFor="let j of starredJokes" [starredJoke]="j"
-                           jokeDeleteEvent="unstarJoke($event)"
-                             jokeUnstarEvent="unstarJoke($event)"></app-joke-starred-item>
+      <app-joke-starred-item *ngFor="let j of jokes" [joke]="j" (jokeUnstarEvent)="unstarJoke($event)"></app-joke-starred-item>
     </div>`,
   styleUrls: ['joke.component.styl']
 })
 export class JokeStarredListComponent implements OnInit {
-
-  private storageKey = 'starredJokes';
-  starredJokes: Joke[];
+  @Output() jokeListUnstarEvent = new EventEmitter<Joke>();
+  @Input() jokes: Joke[];
 
   ngOnInit() {
-    try {
-      this.starredJokes = JSON.parse(localStorage.getItem(this.storageKey));
-    } finally {
-      this.starredJokes = [];
-    }
-  }
-
-  starJoke(joke: Joke) {
-    this.starredJokes.unshift(joke);
-    localStorage.setItem(this.storageKey, JSON.stringify(this.starredJokes));
   }
 
   unstarJoke(joke: Joke) {
-    const i = this.starredJokes.indexOf(joke);
-    if (i) {
-      this.starredJokes.splice(i, 1);
-    }
+    this.jokeListUnstarEvent.emit(joke);
   }
-
 }
 
 @Component({
   selector: 'app-joke-list',
   template: `
-    <app-joke-starred-list jokeStarEvent="starJoke($event)"></app-joke-starred-list>
-    <app-joke-form jokeCreatedEvent="addJoke($event)"></app-joke-form>
-    <app-joke-item *ngFor="let j of jokes" [joke]="j" jokeDeleteEvent="deleteJoke($event)"></app-joke-item>`,
+    <app-joke-starred-list  [jokes]="starredJokes" (jokeListUnstarEvent)="unstarJoke($event)"></app-joke-starred-list>
+    <app-joke-form (jokeCreatedEvent)="addJoke($event)"></app-joke-form>
+    <app-joke-item *ngFor="let j of jokes" [joke]="j" (jokeStarEvent)="starJoke($event)" (jokeDeleteEvent)="deleteJoke($event)"></app-joke-item>`,
   styleUrls: ['joke.component.styl']
 })
-export class JokeComponent implements OnInit {
-
+export class JokeComponent implements OnInit, OnDestroy {
+  private storageKey = 'jokes';
   jokes: Joke[];
+  starredJokes: Joke[];
 
   ngOnInit() {
     this.jokes = JOKES;
+    try {
+      this.starredJokes = JSON.parse(localStorage.getItem(this.storageKey));
+    } catch (e) {
+      console.debug(e);
+      this.starredJokes = [];
+    } finally { }
+  }
+
+  ngOnDestroy(): void {
+    this.saveStarredJokes();
   }
 
   addJoke(joke: Joke) {
@@ -144,7 +138,30 @@ export class JokeComponent implements OnInit {
   }
 
   deleteJoke(joke: Joke) {
+    this.unstarJoke(joke);
     this.jokes.splice(this.jokes.indexOf(joke), 1);
   }
 
+  starJoke(joke: Joke) {
+    const idx = this.starredJokes.indexOf(joke);
+    if (idx < 0) {
+      this.starredJokes.unshift(joke);
+      this.saveStarredJokes();
+    }
+  }
+
+  unstarJoke(joke: Joke) {
+    const idx = this.starredJokes.indexOf(joke);
+    if (idx >= 0) {
+      this.starredJokes.splice(idx, 1);
+      this.saveStarredJokes();
+    }
+  }
+
+  saveStarredJokes() {
+    if (localStorage.getItem(this.storageKey)) {
+      localStorage.removeItem(this.storageKey);
+    }
+    localStorage.setItem(this.storageKey, JSON.stringify(this.starredJokes));
+  }
 }
